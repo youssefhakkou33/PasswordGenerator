26055a6303c1622cc6528f89c7b17dac13501999
youssefhakkou33 <youssef.hakkou33@gmail.com>
2023-11-11 21:23:28 -0800

    Added code to generate a 13 char string

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,3 +1,10 @@
 import random
+import string
+from os import system
+import secrets
 
+system("clear")
+alphabet = string.ascii_letters + string.digits
+random_string = ''.join((secrets.choice(alphabet)) for i in range(13))
 
+print("Here is your random password: ", random_string)
